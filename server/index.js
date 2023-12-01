61b3ee2f8a330d15eaa2d230dadea1de20fc906b
Marian Israel <marianwadia55@gmail.com>
2023-12-01 05:08:30 +0200

    added header && clients components

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
 const colors = require('colors');
@@ -10,6 +11,7 @@ const app = express();
 
 connectDB()
 
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'DEVELOPMENT' ? true : false
