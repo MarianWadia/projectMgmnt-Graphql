4220ea785ecd27b4977f6368640fc784a67de460
Marian Israel <marianwadia55@gmail.com>
2023-12-01 05:07:31 +0200

    added header && clients components

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import Clients from "./components/Clients"
+import Header from "./components/Header"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client" 
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache()
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client} >
+      <Header />
+      <Clients />
+    </ApolloProvider>
+  )
+}
+
+export default App
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+  return (
+    <div className="bg-gray-100 p-1">
+       <div className="flex flex-row gap-2 mx-8 md:mx-24 items-center">
+            <div className="w-9 h-9">
+                    <img src="https://bs-uploads.toptal.io/blackfish-uploads/skill_page/content/logo_file/logo/6212/GraphQL_Logo.svg-490ae3deb7c0f056c849d7463fb8ab39.png" 
+                        alt="graphql-logo"
+                    />
+            </div>
+                <h1 className="text-pink-500 text-2xl cursor-pointer hover:text-black">ProjectMgmt</h1>
+       </div>
+    </div>
+  )
+}
+
+export default Header
